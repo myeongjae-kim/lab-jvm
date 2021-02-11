95fe85c2d22853fbdccf6c22e59197d4c152c037
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2021-02-11 15:42:44 +0900

    [#1] rename test class

--- spring-web-kotlin-blocking/src/intTest/kotlin/kim/myeongjae/spring_web_kotiln_blocking/SpringWebKotlinBlockingApplicationIntTest.kt
+++ spring-web-kotlin-blocking/src/intTest/kotlin/kim/myeongjae/spring_web_kotiln_blocking/SpringWebKotlinBlockingApplicationIntTest.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class SpringWebKotlinBlockingApplicationTest {
+class SpringWebKotlinBlockingApplicationIntTest {
 
     @Test
     fun contextLoads() {
