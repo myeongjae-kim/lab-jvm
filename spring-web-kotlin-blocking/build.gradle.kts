d0e73ab9f50b15f8437d7f7bd33198fdd43f053b
김명재 Myeongjae Kim <dev@myeongjae.kim>
2023-12-10 20:09:40 +0900

    feat: json deserialize for value class

--- spring-web-kotlin-blocking/build.gradle.kts
+++ spring-web-kotlin-blocking/build.gradle.kts
@@ -3,6 +3,10 @@ plugins {
     kotlin("plugin.allopen")
 }
 
+repositories {
+    maven { setUrl("https://jitpack.io") }
+}
+
 allOpen {
     annotation("jakarta.persistence.Entity")
     annotation("jakarta.persistence.Embeddable")
@@ -15,6 +19,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    // https://github.com/FasterXML/jackson-module-kotlin/issues/199#issuecomment-1427990047
+    implementation("com.github.ProjectMapK:jackson-module-kogera:2.16.0-beta8")
     runtimeOnly("com.h2database:h2")
     runtimeOnly("mysql:mysql-connector-java:8.0.33")
 
--- spring-web-kotlin-blocking/src/test/kotlin/kim/myeongjae/springwebkotilnblocking/article/domain/model/ArticleIdTest.kt
+++ spring-web-kotlin-blocking/src/test/kotlin/kim/myeongjae/springwebkotilnblocking/article/domain/model/ArticleIdTest.kt
@@ -1,20 +1,49 @@
 package kim.myeongjae.springwebkotilnblocking.article.domain.model
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.fasterxml.jackson.databind.exc.InvalidDefinitionException
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import io.github.projectmapk.jackson.module.kogera.readValue
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
+import io.github.projectmapk.jackson.module.kogera.jacksonObjectMapper as jacksonObjectMapperKogera
 
 private class ExampleResponse(val id: ArticleId)
+data class ExampleRequest(val id: ArticleId = ArticleId(0L), val foo: String = "")
 
-class ArticleIdTest : StringSpec({
+class ArticleIdTest : StringSpec(
+    {
+        val objectMapperKogera = jacksonObjectMapperKogera()
+        val objectMapper = jacksonObjectMapper()
 
-    "serialize" {
-        val objectMapper = ObjectMapper().apply {
-            registerModule(KotlinModule.Builder().build())
+        "serialize" {
+            objectMapper.writeValueAsString(ArticleId(1L)) shouldBe "1"
+            objectMapper.writeValueAsString(ExampleResponse(ArticleId(1L))) shouldBe "{\"id\":1}"
         }
 
-        objectMapper.writeValueAsString(ArticleId(1L)) shouldBe "1"
-        objectMapper.writeValueAsString(ExampleResponse(ArticleId(1L))) shouldBe "{\"id\":1}"
-    }
-},)
+        "deserialize_fail" {
+            shouldThrow<InvalidDefinitionException> {
+                objectMapper.readValue<ExampleRequest>(
+                    """
+                {
+                  "id": 1,
+                  "foo": "bar"
+                }
+                    """.trimIndent(),
+                )
+            }
+        }
+
+        "deserialize_kogera" {
+            val obj: ExampleRequest = objectMapperKogera.readValue(
+                """
+                {
+                  "id": 1,
+                  "foo": "bar"
+                }
+                """.trimIndent(),
+            )
+            obj shouldBe ExampleRequest(ArticleId(1L), "bar")
+        }
+    },
+)
